336848462b481af2334cbab960095f7c431a31b8
KachanVlg <vlad.bend@inbox.ru>
2025-01-20 22:30:24 +0300

    feat - Taco.java prepared for storage in the database

--- src/main/java/sia/tacocloud/domainEntities/Taco.java
+++ src/main/java/sia/tacocloud/domainEntities/Taco.java
@@ -4,10 +4,16 @@ package sia.tacocloud.domainEntities;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+    private Long id;
+
+    private Date createdAt = new Date();
 
     @NotNull
     @Size(min = 5, message="Name must be at least 5 characters long")
