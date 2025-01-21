1ba1b2a0dbf62329a87b4c23db759ea2b502b929
KachanVlg <vlad.bend@inbox.ru>
2025-01-21 21:11:44 +0300

    feat - create possibility save tacos into db in controller

--- src/main/java/sia/tacocloud/controllers/OrderController.java
+++ src/main/java/sia/tacocloud/controllers/OrderController.java
@@ -3,6 +3,7 @@ package sia.tacocloud.controllers;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.domainEntities.TacoOrder;
+import sia.tacocloud.repositories.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+
+    private OrderRepository orderRepo;
+
+    @Autowired
+    public  OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     @GetMapping("/current")
     public String orderForm() {
@@ -26,7 +36,7 @@ public class OrderController {
     @PostMapping
     public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
         if (errors.hasErrors()) {return "orderForm";}
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
     }
