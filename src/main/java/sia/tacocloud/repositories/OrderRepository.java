a07f5da0cbafae7a2ad317e2852aaab4c3f80b50
KachanVlg <vlad.bend@inbox.ru>
2025-01-21 19:14:28 +0300

    feat - create OrderRepository

---
+++ src/main/java/sia/tacocloud/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package sia.tacocloud.repositories;
+
+
+import sia.tacocloud.domainEntities.Taco;
+import sia.tacocloud.domainEntities.TacoOrder;
+
+public interface OrderRepository {
+
+    TacoOrder save(TacoOrder order);
+}
