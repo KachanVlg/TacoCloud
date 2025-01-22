a760c1f49090dcd1e35dfa7d1af3eb41cd671488
KachanVlg <vlad.bend@inbox.ru>
2025-01-22 23:01:50 +0300

    mod - simplified OrderRepository

--- src/main/java/sia/tacocloud/repositories/OrderRepository.java
+++ src/main/java/sia/tacocloud/repositories/OrderRepository.java
@@ -1,9 +1,8 @@
 package sia.tacocloud.repositories;
 
 
+import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.domainEntities.Taco;
 import sia.tacocloud.domainEntities.TacoOrder;
 
-public interface OrderRepository {
-    TacoOrder save(TacoOrder order);
-}
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {}
