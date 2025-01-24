2e85ffc8492630c7a6cb81333f401268679a8a18
KachanVlg <vlad.bend@inbox.ru>
2025-01-24 20:44:41 +0300

    mod - jdbc to jpa

--- src/main/java/sia/tacocloud/domainEntities/Taco.java
+++ src/main/java/sia/tacocloud/domainEntities/Taco.java
@@ -1,29 +1,40 @@
 package sia.tacocloud.domainEntities;
 
 
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import org.springframework.data.annotation.Id;
+import lombok.*;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PUBLIC, force = true)
 public class Taco {
 
-    @Id
-    private Long id;
 
-    private Date createdAt = new Date();
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final Long id;
+
+    private final Date createdAt = new Date();
 
     @NotNull
     @Size(min = 5, message="Name must be at least 5 characters long")
-    private String name;
+    private final String name;
 
     @NotNull
     @Size(min=1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    @ManyToMany
+    private final List<Ingredient> ingredients;
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
