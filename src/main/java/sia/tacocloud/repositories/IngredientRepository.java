52ab20630d7633dde7b53103d31ca16c449ac5d2
KachanVlg <vlad.bend@inbox.ru>
2025-01-24 20:53:44 +0300

    mod - jdbc to jpa

--- src/main/java/sia/tacocloud/repositories/IngredientRepository.java
+++ src/main/java/sia/tacocloud/repositories/IngredientRepository.java
@@ -3,6 +3,5 @@ package sia.tacocloud.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.domainEntities.Ingredient;
 
-import java.util.Optional;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {}
--- src/main/java/sia/tacocloud/repositories/OrderRepository.java
+++ src/main/java/sia/tacocloud/repositories/OrderRepository.java
@@ -2,7 +2,6 @@ package sia.tacocloud.repositories;
 
 
 import org.springframework.data.repository.CrudRepository;
-import sia.tacocloud.domainEntities.Taco;
 import sia.tacocloud.domainEntities.TacoOrder;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {}
