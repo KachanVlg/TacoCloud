66421759009f5f783fd5a530b1d70e1cb2e6706b
KachanVlg <vlad.bend@inbox.ru>
2025-01-26 01:45:09 +0300

    feat - redact homepage, create login page

--- src/main/java/sia/tacocloud/config/WebConfig.java
+++ src/main/java/sia/tacocloud/config/WebConfig.java
@@ -1,13 +1,16 @@
 package sia.tacocloud.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login");
+        registry.addViewController("/login").setViewName("login");
     }
 }
